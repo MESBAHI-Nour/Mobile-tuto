f43b38eb1d590d9ae6499d5de6a5200751a88a72
MESBAHI-Nour <s3487637@email.com>
2026-09-23 13:36:07 +0000

    tuto 8 done

---
+++ Structurer-application-en-composants/3-Structurer-et-verifier-API/frontend/app.js
@@ -0,0 +1,11 @@
+fetch('../backend/categories.php')
+    .then(response => response.json())
+    .then(categories => {
+        const ul = document.getElementById('liste-categories');
+        categories.forEach(categorie => {
+            const li = document.createElement('li');
+            li.textContent = categorie.nom;
+            ul.appendChild(li);
+        });
+    })
+    .catch(erreur => console.error("Erreur de communication :", erreur));
